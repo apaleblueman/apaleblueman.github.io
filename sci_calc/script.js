d8be8a6c4da1f466c149725900b06092bad504e5
apaleblueman <gurchetan.singh2@icloud.com>
2024-01-29 14:16:55 -0800

    Add front-end for sci calculator

---
+++ sci_calc/script.js
@@ -0,0 +1,10 @@
+function parse_button_val(operator_id){
+    let b = document.querySelector(operator_id);
+    b.innerHTML
+}
+
+
+//use dom query to get button val 
+//then put that button val in textinput field
+//
+//
